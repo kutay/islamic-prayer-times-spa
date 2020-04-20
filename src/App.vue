24edeeadb6194cf98f94037f9ae5d67833d7935d
Aykut Açikel <aykut.acikel@gmail.com>
2020-04-20 23:03:51 +0200

    Add clock on top right

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+
+        <b-navbar id="nav">
+            <template slot="start">
+                <b-navbar-item href="#">
+                    <router-link to="/">Home</router-link>
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    <router-link to="/about">About</router-link>
+                </b-navbar-item>
+            </template>
+
+            <template slot="end">
+                <b-navbar-item tag="div">
+                    <div class="buttons">
+                        <digital-clock :blink="false" :displaySeconds="true"/>
+                    </div>
+                </b-navbar-item>
+            </template>
+        </b-navbar>
+
+        <hr/>
+
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    import DigitalClock from "vue-digital-clock";
+
+    export default {
+        components: {
+            DigitalClock
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #nav {
+        padding: 10px 10px 0px;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .clock {
+        /*background-color: #263238;*/
+        /*color: #eceff1;*/
+        padding: 0.3rem 0.6rem;
+        font-size: 2rem;
+        font-family: "Menlo", monospace;
+    }
 </style>
