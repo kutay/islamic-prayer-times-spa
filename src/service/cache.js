dc2bf5ced55ba59bf3b5f2af43855c7c9c63279c
Aykut Açikel <aykut.acikel@gmail.com>
2020-04-21 22:07:28 +0200

    Add very basic cache for geocoding with localStorage

---
+++ src/service/cache.js
@@ -0,0 +1,32 @@
+
+const CACHE_DATA_KEY = "cache_data";
+
+if (!localStorage.getItem(CACHE_DATA_KEY)){
+    localStorage.setItem(CACHE_DATA_KEY, JSON.stringify({}));
+}
+
+const cacheData = JSON.parse(localStorage.getItem(CACHE_DATA_KEY));
+
+// eslint-disable-next-line no-unused-vars
+function sync() {
+    localStorage.setItem(CACHE_DATA_KEY, JSON.stringify(cacheData))
+}
+
+function exists(key) {
+    return !!cacheData[key];
+}
+
+function retrieve(key) {
+    return cacheData[key];
+}
+
+function save(key, value) {
+    cacheData[key] = value;
+    sync();
+}
+
+export default {
+    exists,
+    retrieve,
+    save
+}
--- src/service/geocoding.js
+++ src/service/geocoding.js
@@ -1,10 +1,22 @@
 const axios = require("axios");
 
+import cache from "./cache";
+
 // https://geo.api.gouv.fr/adresse
 
 async function reverseGeocode(lat, lon) {
-    const result = await axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lon}&lat=${lat}`);
-    return result.data.features[0];
+    const cacheKey = `reverse/${lat}/${lon}`;
+
+    if (cache.exists(cacheKey)) {
+        return cache.retrieve(cacheKey);
+    }
+
+    const results = await axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lon}&lat=${lat}`);
+    const result = results.data.features[0];
+
+    cache.save(cacheKey, result);
+
+    return result;
 }
 
 export default {
